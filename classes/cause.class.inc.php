95da5014a0f6fd51abe84ee215069f47415c2d7a
stowka <antoine@netproduction.fr>
2013-12-11 09:35:13 +0100

    Class Cause

---
+++ classes/cause.class.inc.php
@@ -0,0 +1,70 @@
+<?php
+	/*
+	 * @author Antoine De Gieter
+	 *
+	 * Class Cause: cause of disagreement.
+	 *
+	 */
+	class Cause {
+		private $id;
+		private $cause;
+		private $db;
+
+		public function __construct( $id ) {
+			$id || die( "Error: Wrong cause." );
+			$this->db = $_SESSION['db'];
+			$this->fetchData( $id );
+		}
+
+		private function fetchData( $id ) {
+			$stmt = $this->db->prepare( "select * from cause where id = ?" );
+			$stmt->execute( array(
+				$id
+			) );
+			$type = $stmt->fetch(PDO::FETCH_ASSOC);
+			$stmt->closeCursor();
+			$this->label = $type['cause'];
+		}
+
+		/*
+		 * ===
+		 * MAGIC METHODS
+		 * ===
+		 */
+
+		/**
+		 * @Override
+		 * toString method
+		 */
+		public function __toString() {
+			return $this->cause;
+		}
+
+		/*
+		 * ===
+		 * GETTERS
+		 * ===
+		 */
+		public function getId() {
+			return $this->id;
+		}
+
+		public function getCause() {
+			return $this->cause;
+		}
+
+		/* ===
+		 * STATIC METHODS
+		 * ===
+		 */
+		public static function all() {
+			$db = $_SESSION['db'];
+			$stmt = $db->prepare( "select id from cause order by cause;" );
+			$stmt->execute();
+			$causes = array();
+			while ( $cause = $stmt->fetch(PDO::FETCH_NUM) )
+				$causes[] = new Cause( $cause[0] );
+			$stmt->closeCursor();
+			return $causes;
+		}
+	}
