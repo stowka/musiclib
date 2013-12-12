3f603fb7843b4afc5987b60e04a81c489a0bf59d
Eska7 <alexis.beaujon@gmail.com>
2013-12-12 17:16:30 +0100

    Update add.php

--- add.php
+++ add.php
@@ -17,7 +17,7 @@
 		<?php require_once "sections/menu.php"; ?>
 		<section class="container">
 			<div class="row">
-				<div class="col-md-12">
+				<div class="col-md-7">
 					<ul class="nav nav-tabs" id="myTab">
 						<li class="active nephritis">
 							<a href="#artists" data-toggle="tab">Add artist</a>
@@ -56,11 +56,8 @@
 											<div class="col-md-3">
 												<button type="button" class="btn btn-default btn-lg btn-block" onclick="$('#form-add-artist').submit();">Submit</button>
 												<?php
-													$nameAr = $_POST['nameArtist'];
-													$biography = $_POST['biography'];
-													$user = $user->getId();
-													$picture = $_POST['pictureArtist'];
-													$artist->create( $nameAr, $biography, $user, $picture );
+													if (isset($_POST['nameArtist']) && isset($_POST['biography']) && isset($_POST['pictureArtist']) )
+														Artist::create( $_POST['nameArtist'], $_POST['biography'], user->getId(), $_POST['pictureArtist'] );
 												?>
 											</div>
 										</div>
@@ -72,40 +69,65 @@
 									<fieldset>
 										<p class="padded"></p>
 										<div class="row">
-											<div class="col-md-6">
-												<input type="text" name="name" placeholder="Name">
+											<div class="col-md-7">
+												<input type="text" name="nameAlbum" placeholder="Name">
 											</div>
-											<div class="col-md-6">
-												<input type="file" name="artwork" value="Artwork" required>
+											<div class="col-md-2">
+												<input type="text" name="disc" placeholder="N° Disc" style="font-size: 1.1em; min-height: 39px;">
+											</div>
+											<div class="col-md-3">
+												<input type="text" name="releaseDate" placeholder="Release Date" style="font-size: 1.1em; min-height: 39px;">
 											</div>
 										</div>
-										<br>
+										<br><br>
 										<div class="row">
-											<div class="col-md-2">
-												<input type="text" name="disc" placeholder="Disc">
-											</div>
-											<div class="col-md-4">
-												<input type="text" name="releaseDate" placeholder="Release Date">
-											</div>
-										<!--</div>
-										<br>
-										<div class="row">-->
-											<div class="col-md-4">
+	 										<div class="col-md-4">
 												<select class="form-control alternate" id="listType" name="type" placeholder="type">
-													<option>
-														Single
-													</option>
-													<option>
-														Album
-													</option>
-													<option>
-														Podcast
-													</option>
-													<option>
-														pyqgj
-													</option>
+													<option>Single</option>
+													<option>Album</option>
+													<option>Live</option>
+													<option>Compilation</option>
+													<option>Best Of</option>
+													<option>Studio</option>
+													<option>Podcast</option>
+													<option>pyqgj</option>
 												</select>
 											</div>
+											<div class="col-md-4 col-md-offset-2">
+												<h4>Number of songs</h4>
+											</div>
+											<div class="col-md-1">
+												<button type="button" class="btn btn-default" onclick="moreSong()">+</button>
+											</div>
+											<div class="col-md-1">
+												<button type="button" class="btn btn-default" onclick="lessSong()">-</button>
+											</div>
+										</div>
+										<hr>
+										<div id="songsList">
+										<?php
+											print '<div class="row">';
+												print '<div class="col-md-4">';
+													print '<div class="row">';
+														print '<div class="col-md-1">';
+															print '1';
+														print '</div>';
+														print '<div class="col-md-10">';
+															print '<input type="text" name="duration" placeholder="Duration">';
+														print '</div>';
+													print '</div>';
+													print '<br><span style="font-size:1em;"></span>';
+													print '<div class="row">';
+														print '<div class="col-md-12">';
+															print '<input type="text" name="title" placeholder="Title">';
+														print '</div>';
+													print '</div>';
+												print '</div>';
+												print '<div class="col-md-8">';
+													print '<textarea rows="4" name="lyrics" placeholder="Lyrics"></textarea>';
+												print '</div>';
+											print '</div>';
+										?>
 										</div>
 										<hr>
 										<div class="row">
@@ -115,7 +137,7 @@
 												</p>
 											</div>
 											<div class="col-md-3">
-												<button type="submit" class="btn btn-default btn-lg btn-block">Continue</button>
+												<button type="button" class="btn btn-default btn-lg btn-block">Submit</button>
 											</div>
 										</div>
 									</fieldset>
@@ -124,10 +146,38 @@
 						</div><!--tab-->
 					</div><!--class-->
 				</div><!--.span-->
+				<div class="col-md-5">
+					<div class="alizarin" id="users2">
+						<p class="lead padded">
+							Affiche une image
+						</p>
+						<hr>
+					</div>
+				</div><!--.span-->
 			</div><!-- .row -->
 		</section>
 		<?php
 			require_once "sections/js.php";
 		?>
+
+		<script>
+			nbSong = 1;
+			function moreSong(){
+				nbSong++;
+				xmlhttp = new XMLHttpRequest();
+				xmlhttp.onreadystatechange = function(){
+					if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			    		document.getElementById("songsList").innerHTML += xmlhttp.responseText;
+					}
+				}
+				xmlhttp.open("GET", "add_song.php?n="+nbSong, true);
+				xmlhttp.send();
+			}
+			function lessSong(){
+				document.getElementById("songsAdded"+nbSong).style.display = "none";	
+				nbSong--;
+			}
+		</script>
 	</body>
 </html>
+
