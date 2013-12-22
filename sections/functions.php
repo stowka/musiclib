9c249104a6b30f8b7ae7735d121a304bce3ca2d0
stowka <antoine@netproduction.fr>
2013-12-22 18:55:54 +0100

    functions.php

---
+++ sections/functions.php
@@ -0,0 +1,31 @@
+<?php
+	function truncateTextByWords( $text, $number = 25 ) {
+		$words = explode(" ", $text );
+		$truncatedText = "";
+		for ($i = 0; $i < $number; $i++) :
+			$truncatedText .= $words[$i] . " ";
+		endfor;
+		return $truncatedText . "...";
+	}
+
+	function truncateTextByChars( $text, $number = 32 ) {
+		$truncatedText = substr( $text, 0, $number );
+		if ( $truncatedText < $text )
+			return $truncatedText . "...";
+		return $truncatedText;
+	}
+
+	function normalize ( $string ) {
+		$table = array(
+			'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+			'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+			'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+			'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+			'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+			'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+			'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+			'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r',
+		);
+
+		return strtr( $string, $table );
+	}
